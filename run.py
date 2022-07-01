5c7b5bf3c2642a16c823e569edc8db2be0f6da00
EvergSwe <daniel.nyberg@everg.se>
2022-07-01 15:45:16 +0000

    pep8 input adjustments

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from colorama import Fore, Back, Style
 
 def get_test_subject():
     """
-    Get the subject of the test to be used in end summary 
+    Get the subject of the test to be used in end summary
     """
     test_subject = input("What's the subject for this test? ")
     print(f"Subject for this test is: {test_subject}\n")
@@ -30,13 +30,15 @@ def get_max_test_result():
 
         else:
             print(
-                f"Your input is {max_result} and this is not valid number, please try again: \n")
+                f"Your input is {max_result} and this is not a valid number, please try again: \n")
 
     return int(max_result)
 
+
 def add_student_result(max_result):
     """
-    Add student name and result to dictionary, validate if student result is higher than max result.
+    Add student name and result to dictionary, validate if student
+    result is higher than max result.
     """
     print("Expected input for number of student is number, example 10, 24")
     records = int(input("Enter the number of students: "))
@@ -55,7 +57,8 @@ def add_student_result(max_result):
                 result = float(result_str)
                 break
             except ValueError:
-                print("this is not a valid number, please enter a valid number, example 10 or 9.5")
+                print(
+                    "this is not a valid number, please enter a valid number, example 10 or 9.5")
                 result_str = input(f"\nEnter result for {name}:")
 
         while result > max_result:
@@ -67,11 +70,14 @@ def add_student_result(max_result):
 
     return stud_data
 
+
 def calc_student_performance(data, value):
     """
-    Take the input data from add student data and calculate the student performance based
-    on the max_result. Use the performance and predefined grade levels and create a student
-    test row including name, test result, performance and grade. Return a list of lists.
+    Take the input data from add student data and calculate
+    the student performance based on the max_result. Use the
+    performance and predefined grade levels and create a student
+    test row including name, test result, performance and grade.
+    Return a list of lists.
     """
     students = data
     max_result = int(value)
@@ -105,7 +111,6 @@ def list_data_to_table(list_data):
     """
     converts list data to table and apply sorting on performance column
     """
-
     df = pd.DataFrame(
         list_data, columns=["Name", "Test Result", "Performance", "Grade"])
     perfor_sort = df.sort_values(by="Performance", ascending=False)
@@ -117,6 +122,7 @@ def main():
     """
     Run all program functions
     """
+
     get_test_subject()
     max_result = get_max_test_result()
     stud_data = add_student_result(max_result)
